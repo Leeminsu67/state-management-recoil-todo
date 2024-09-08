15ba7fa00fd6d9f9251f2ebbbf29d00638c9dd94
Leeminsu67 <alstn67@gmail.com>
2024-09-08 23:39:48 +0900

    todo update

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,8 @@
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../Atoms";
 
+// find to do based on id
+// 수정하고자 하는 to do의 경로를 알아야함
 const ToDo = ({ text, category, id }: IToDo) => {
   // typescript tips
   // const onClick = (newCategory: IToDo["category"]) => {
@@ -11,6 +13,13 @@ const ToDo = ({ text, category, id }: IToDo) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log("replace the to do in the index");
+      return oldToDos;
+    });
   };
 
   return (
